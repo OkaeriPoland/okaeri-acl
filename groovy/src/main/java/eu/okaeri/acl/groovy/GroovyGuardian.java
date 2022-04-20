fd7e7d18c1aa104edbf6d929424b78f007e09e3e
PinkLolicorn <pinklolicorngithub@daffit.pl>
2022-04-21 01:55:26 +0200

    Allow priming GroovyGuardian for better performance, release 0.1.3

--- groovy/src/main/java/eu/okaeri/acl/groovy/GroovyGuardian.java
+++ groovy/src/main/java/eu/okaeri/acl/groovy/GroovyGuardian.java
@@ -5,30 +5,70 @@ import eu.okaeri.acl.guardian.Guardian;
 import eu.okaeri.acl.guardian.GuardianContext;
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
+import groovy.lang.Script;
 import lombok.*;
 import org.codehaus.groovy.control.CompilerConfiguration;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Getter
 @Setter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class GroovyGuardian extends Guardian {
 
+    private final Map<String, Script> scriptCache = new ConcurrentHashMap<>();
+
     private CompilerConfiguration compilerConfiguration;
+    private final GroovyShell groovyShell;
 
     public static GroovyGuardian create(@NonNull CompilerConfiguration compilerConfiguration) {
-        return new GroovyGuardian(compilerConfiguration);
+        GroovyGuardian guardian = new GroovyGuardian(compilerConfiguration, new GroovyShell(compilerConfiguration));
+        guardian.groovyShell.evaluate("return 1"); // prime shell for faster response time later
+        return guardian;
     }
 
     public static GroovyGuardian create() {
-        return new GroovyGuardian(new CompilerConfiguration());
+        return create(new CompilerConfiguration());
     }
 
     @Override
     public String evaluate(@NonNull Guard guard, @NonNull GuardianContext context) {
 
-        Binding binding = new Binding(context.getData());
-        GroovyShell groovyShell = new GroovyShell(binding, this.compilerConfiguration);
+        Script script = this.scriptCache.computeIfAbsent(guard.value(), this.groovyShell::parse);
+        script.setBinding(new Binding(context.getData()));
 
-        return String.valueOf(groovyShell.evaluate(guard.value()));
+        return String.valueOf(script.run());
+    }
+
+    public long prime(@NonNull Class<?> clazz) {
+
+        long primed = this.prime(clazz.getAnnotationsByType(Guard.class));
+
+        for (Method method : clazz.getMethods()) {
+            primed += this.prime(method.getAnnotationsByType(Guard.class));
+        }
+
+        for (Method declaredMethod : clazz.getDeclaredMethods()) {
+            primed += this.prime(declaredMethod.getAnnotationsByType(Guard.class));
+        }
+
+        return primed;
+    }
+
+    public long prime(@NonNull Guard[] annotations) {
+        return Arrays.stream(annotations)
+            .filter(annotation -> this.prime(annotation.value()))
+            .count();
+    }
+
+    public boolean prime(@NonNull String expression) {
+        if (this.scriptCache.containsKey(expression)) {
+            return false;
+        }
+        this.scriptCache.put(expression, this.groovyShell.parse(expression));
+        return true;
     }
 }
